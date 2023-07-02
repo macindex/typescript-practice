c7f81d47e053f1316debb450f3700bd58d44f304
macindex <machollanda@gmail.com>
2023-07-02 17:10:54 -0300

    functions

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,3 +1,75 @@
 "use strict";
 let x = 10;
 x = 16;
+console.log(x);
+// inference 
+let y = 12;
+// annotation
+let z = 12;
+// Basic types
+let firstName = "claudio";
+let age = 30;
+const isAdmin = true;
+// Object
+const myNumber = [1, 2, 3];
+// Tuplas
+let myTuple;
+myTuple = [5, "teste1", ["a", "b"]];
+// myTuple = [true, false, true]
+// Object literals 
+const user = {
+    name: "Claudio",
+    age: 20
+};
+// Any
+// bad practice
+let a = 0;
+a = "teste";
+a = true;
+a = 23;
+a = [];
+// Union type
+let id = "10";
+const userId = 10;
+const productId = "10";
+const shirtId = 123;
+// Enum
+var Size;
+(function (Size) {
+    Size["P"] = "Small";
+    Size["M"] = "Medium";
+    Size["G"] = "Big";
+})(Size || (Size = {}));
+const shirt = {
+    name: "Shirt v",
+    size: Size.G
+};
+// Literal types
+let teste;
+teste = "autenticated";
+teste = null;
+// Functions
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(3, 2));
+function sayHello(name) {
+    return `Hello ${name}`;
+}
+console.log(sayHello("Claudio"));
+function logger(msg) {
+    console.log(msg);
+}
+logger("Teste!");
+function greeting(name, greet) {
+    if (greet) {
+        console.log(`Olá ${greet} ${name}`);
+    }
+    else {
+        console.log(`Olá ${name}`);
+    }
+}
+greeting("jose");
+greeting("jose", "Sir");
+// Interfaces
+// export {};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-let x: number = 10;
+let a: number = 10;
 
-x = 16;
+a = 16;
 
 console.log(x);
 
@@ -11,4 +11,107 @@ let y = 12;
 // annotation
 let z:number = 12;
 
-export {};
+// Basic types
+
+let firstName: string = "claudio";
+let age: number = 30;
+const isAdmin: boolean = true;
+
+// Object
+const myNumber: number[] = [1,2,3];
+
+// Tuplas
+let myTuple: [number, string, string[]];
+
+
+myTuple = [5, "teste1", ["a", "b"]]
+// myTuple = [true, false, true]
+
+// Object literals 
+const user: {name: string, age: number} = {
+    name: "Claudio",
+    age: 20
+}
+
+// Any
+// bad practice
+
+let b:any = 0;
+
+b = "teste";
+b = true;
+b = 23;
+b = [];
+
+// Union type
+
+let id: string | number = "10"
+
+
+// Type alias
+
+type myIdType = number | string 
+
+const userId: myIdType = 10;
+const productId: myIdType = "10";
+const shirtId: myIdType = 123;
+
+
+// Enum
+
+enum Size {
+    P = "Small",
+    M = "Medium",
+    G = "Big"
+}
+
+const shirt = {
+    name: "Shirt v",
+    size: Size.G
+}
+
+// Literal types
+
+let teste: "autenticated" | null;
+
+teste = "autenticated";
+teste = null;
+
+// Functions
+
+function sum(a: number, b: number){
+    return a + b;
+}
+
+console.log(sum(3, 2));
+
+function sayHelloOne(name: string): string {
+    return `Hello ${name}`;
+}
+
+console.log(sayHelloOne("Claudio"));
+
+function logger(msg: string): void{
+	console.log(msg);
+}
+
+logger("Teste!");
+
+function greeting(name: string, greet ?: string ){
+    if(greet){
+        console.log(`Olá ${greet} ${name}`);
+    }else{
+        console.log(`Olá ${name}`)
+    }
+}
+
+greeting("jose");
+greeting("jose", "Sir");
+
+// Interfaces
+
+
+
+
+
+// export {};
